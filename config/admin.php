fabbde9ab72eece9930db624f213fa0d6ee83f15
l-deie <xuningandhua717@163.com>
2021-05-12 10:09:29 +0800

    config

---
+++ config/admin.php
@@ -0,0 +1,13 @@
+<?php
+return array(		
+//模块配置
+'MODULE_PATH'=>'./module/admin/',//模块存放目录，一般不需要修改
+//模板配置
+'TPL_TEMPLATE_PATH'=>'./template/admin/',//模板目录，一般不需要修改
+//指定成功页面和错误页面模板
+//'TPL_ACTION_SUCCESS'=>'success',//成功页面模板
+//'TPL_ACTION_ERROR'=>'error',//错误页面模板
+//'URL_HTTP_HOST'=>'',//域名网址
+);
+
+?>
